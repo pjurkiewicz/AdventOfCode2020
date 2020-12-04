955653df8a07a51988378cf1537c53177a9defc5
Piotr Jurkiewicz <pjurkiew@blk893.softservecom.com>
2020-12-04 01:28:46 +0100

    added day03 part2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,14 +1,16 @@
-use std::collections::HashSet;
 use std::fs;
 
 fn main() {
-    let  input_data : Vec<String> = get_input_data("input.txt");
+    let input_data: Vec<String> = get_input_data("input.txt");
     let part1_result = solve_part1(&input_data);
     println!("part1: {}", part1_result);
+
+    let part2_result = solve_part2(&input_data);
+    println!("part2: {}", part2_result);
 }
 
 fn get_input_data(input_file_mame: &str) -> Vec<String> {
-    let mut file_content =
+    let file_content =
         fs::read_to_string(input_file_mame).expect("something went wrong reading file");
     let values: Vec<String> = file_content.lines().map(String::from).collect();
 
@@ -16,19 +18,44 @@ fn get_input_data(input_file_mame: &str) -> Vec<String> {
 }
 
 fn solve_part1(input_data: &Vec<String>) -> u32 {
+    tree_finder(input_data, 3, 1)
+}
+
+fn solve_part2(input_data: &Vec<String>) -> usize {
+    let trees: Vec<u32> = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+        .iter()
+        .map(|a| tree_finder(input_data, a.0, a.1))
+        .collect();
+
+    let mut result: usize = 1;
+    for elem in trees {
+        result *= elem as usize;
+    }
+
+    result
+}
+
+fn tree_finder(
+    input_data: &Vec<String>,
+    horizontal_steps_count: usize,
+    vertical_steps_count: usize,
+) -> u32 {
     let mut row_pos: usize = 0;
-    const horizontal_steps_count: usize = 3;
 
     let mut line_number: usize = 0;
     let mut tree_count: u32 = 0;
     for line in input_data {
+        if line_number % vertical_steps_count > 0 {
+            line_number +=1;
+            continue;
+        }
+
         let line_chars: Vec<char> = line.chars().collect();
         if line_chars.len() <= row_pos {
             row_pos = row_pos - line_chars.len();
         }
         if line_chars[row_pos] == '#' {
             tree_count += 1;
-            println!("lien {} pos {}", line_number, row_pos);
         }
 
         row_pos += horizontal_steps_count;
@@ -63,4 +90,29 @@ fn solve_part1_works() {
     let result = solve_part1(&input_data);
 
     assert_eq!(result, 7);
+}
+
+#[test]
+fn solve_part2_works() {
+    let input_data: Vec<String> = [
+        "..##.........##.........##.........##.........##.........##.......",
+        "#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..",
+        ".#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.",
+        "..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#",
+        ".#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.",
+        "..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....",
+        ".#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#",
+        ".#........#.#........#.#........#.#........#.#........#.#........#",
+        "#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...",
+        "#...##....##...##....##...##....##...##....##...##....##...##....#",
+        ".#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#",
+    ]
+    .iter()
+    .cloned()
+    .map(String::from)
+    .collect();
+
+    let result = solve_part2(&input_data);
+
+    assert_eq!(result, 336);
 }
